ebc30e9b594699687df113515927d630b4752ee6
Arif Alan <arifalan001@hotmail.com>
2022-08-24 20:32:35 +0300

    Add project files.

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+        readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+
+        public ProductsController(
+            IProductReadRepository productReadRepository,
+            IProductWriteRepository productWriteRepository,
+            IOrderWriteRepository orderWriteRepository,
+            IOrderReadRepository orderReadRepository,
+            ICustomerWriteRepository customerWriteRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _orderReadRepository = orderReadRepository;
+            _customerWriteRepository = customerWriteRepository;
+        }
+        [HttpGet]
+        public async Task Get()
+        {
+          Order order =await _orderReadRepository.GetByIdAsync("f19f56aa-3a01-474b-9301-7da0c30f9b2d");
+
+            order.Address = "istanbul";
+           await _orderWriteRepository.SaveAsync();
+        }
+
+     
+    }
+}
